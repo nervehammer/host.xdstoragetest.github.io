73d42db46bb467cf91952d65543553edd06c477a
nervehammer <suraj.raw120@gmail.com>
2019-05-14 15:10:23 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const crel = require('crel');
+const doc = require('doc-js');
+
+const instructions = crel('div', {
+    class: 'instructions',
+},
+    crel('h3', 'cross-domain-storage host')
+);
+
+doc.ready(function () {
+    crel(document.body,
+        instructions
+    );
+
+    window.localStorage.setItem('foo', JSON.stringify('ezpzls'));
+
+    const storageHost = createStorageHost([
+        {
+            origin: 'http://guest.xdstoragetest.github.io',
+            allowedMethods: ['get', 'set', 'remove'],
+        },
+    ]);
+
+});
